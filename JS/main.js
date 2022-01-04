bf47e7a101c7a622cc8fd8cd35f13cdefbe7a8ab
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-01-05 00:40:07 +0200

    Edited Main Page

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,15 @@ let menuCloseBtn = document.querySelector("#close");
 let nav = document.querySelector("nav");
 
 menuBtn.onclick = function () {
-  nav.style.cssText += "right: 28px";
+  nav.style.cssText += "width: 70vw;";
 };
 
 menuCloseBtn.onclick = function () {
-  nav.style.cssText += "right: -100%";
+  nav.style.cssText += "width: 0vw;";
 };
 
 window.onresize = function () {
   if (window.innerWidth > 700) {
-    nav.style.cssText += "right: 0";
+    nav.style.cssText += "right: 0; width: auto;";
   }
 };
