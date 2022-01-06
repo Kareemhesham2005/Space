135f2acac3e404a55e0c17d1777eca27fd8c84f9
Kareem Hesham <85392795+Kareemhesham2005@users.noreply.github.com>
2022-01-06 02:04:00 +0200

    Customised Technology Pages

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,9 @@ let menuCloseBtn = document.querySelector("#close");
 let nav = document.querySelector("nav");
 let closeBtn = document.querySelector("nav img");
 let menu = document.querySelector("nav ul");
+let img = document.querySelector("#tech .content .image img");
+let arrayOfPages = ["SPACEPORT", "LAUNCH VEHICLE", "SPACE CAPSULE"];
+let heading = document.querySelector("#tech .content .text h1");
 
 menuBtn.onclick = function () {
   nav.style.cssText += "width: 70vw;";
@@ -26,3 +29,30 @@ window.onresize = function () {
     closeBtn.style.display = "none";
   }
 };
+let src_portrait;
+let src_landscape;
+
+if (heading.innerText == arrayOfPages[0]) {
+  src_portrait = "/Images/image-spaceport-portrait.jpg";
+  src_landscape = "/Images/image-spaceport-landscape.jpg";
+} else if (heading.innerText == arrayOfPages[1]) {
+  src_portrait = "/Images/image-launch-vehicle-portrait.jpg";
+  src_landscape = "/Images/image-launch-vehicle-landscape.jpg";
+} else {
+  src_portrait = "/Images/image-space-capsule-portrait.jpg";
+  src_landscape = "/Images/image-space-capsule-landscape.jpg";
+}
+
+if (window.innerWidth <= 1390) {
+  img.setAttribute("src", src_landscape);
+} else {
+  img.setAttribute("src", src_portrait);
+}
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth <= 1390) {
+    img.setAttribute("src", src_landscape);
+  } else {
+    img.setAttribute("src", src_portrait);
+  }
+});
